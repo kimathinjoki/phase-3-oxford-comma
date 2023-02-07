4f994f80e8c1f6c7a523b3f6bb3a1228622690fc
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-07 10:36:21 -0800

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def oxford_comma(arr)
 
+    if arr.length == 2
+        arr.join(" and ")
+    elsif arr.length > 2
+        first_strings = arr[0..arr.length-2].join(", ")
+        second_strings = arr[-1].join(", ")
+        first_strings +" "+ second_strings
+    else
+        arr.join("")
+   end
 end
