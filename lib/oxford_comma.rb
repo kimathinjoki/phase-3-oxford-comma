af9e5ed6a74c571a441b1bd54c7a77e9421d5076
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-08 02:35:22 -0800

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(arr)
         arr.join(" and ")
     elsif arr.length > 2
         first_strings = arr[0..arr.length-2].join(", ")
-        second_strings = arr[-1].join(", ")
-        first_strings + " " + second_strings
+        second_strings = arr[-1].to_s
+        first_strings + ", and " + second_strings
     else
         arr.join("")
    end
